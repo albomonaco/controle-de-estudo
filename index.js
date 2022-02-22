37dd41249dde647b51d5c8edc47836b487d3a48e
AlboMonco <albomonaco@gmail.com>
2022-02-22 11:54:07 -0300

    Added comments on code

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function diaDaSemana(){
-    let time = new Date().getTime()/(1000*60*60*24);
-    let timeFixed = Math.floor(time);
+    let time = new Date().getTime()/(1000*60*60*24); //converting time from miliseconds to days since Jan 1st 1970
+    let timeFixed = Math.floor(time); //rounding
     let listaDeDias = ["Quinta","Sexta","Sábado","Domingo", "Segunda", "Terça", "Quarta"];
     
-    return listaDeDias[timeFixed%7];   
+    return listaDeDias[timeFixed%7]; //returning day of the week based on OS
 }
 
 function materiasAEstudar(dict, dia){
-    return dict[`${dia}`];
+    return dict[`${dia}`]; //return subjects from "materias" 
 }
 
 let dia = diaDaSemana();
