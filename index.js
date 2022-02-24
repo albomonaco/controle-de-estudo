4f50ea066cd52d0069a462e68ab555d2f227613e
AlboMonco <albomonaco@gmail.com>
2022-02-24 14:16:01 -0300

    Code refactored

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function diaDaSemana(){
-    let time = new Date().getTime()/(1000*60*60*24); //converting time from miliseconds to days since Jan 1st 1970
-    let timeFixed = Math.floor(time); //rounding
     let listaDeDias = ["Quinta","Sexta","Sábado","Domingo", "Segunda", "Terça", "Quarta"];
-    
     return listaDeDias[timeFixed%7]; //returning day of the week based on OS
 }
 
 function materiasAEstudar(dict){
     return dict[`${diaDaSemana()}`]; //return subjects from "materias" 
 }
+
+let time = new Date().getTime()/(1000*60*60*24); //converting time from miliseconds to days since Jan 1st 1970
+let timeFixed = Math.floor(time); //rounding
 
 let materias = {
     Quinta: "Matemática, Filosofia e Sociologia",
@@ -18,7 +18,7 @@ let materias = {
     Segunda:  "Matemática e Português",
     Terça: "Física, História e Geografia", 
     Quarta: "Química e Português"
-};
+}; //subjects
 
 console.log(`As matérias que você tem que estudar hoje, ${diaDaSemana()}, são:\n${materiasAEstudar(materias)}`);
 
